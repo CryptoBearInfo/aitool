0f02955cd33a08f11d1ff40831877a4dce2ed85e
Yuejia Xiang <xiangyuejia@qq.com>
2020-10-23 17:19:16 +0800

    init repository/dataset

---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 2020/10/23
+# @Author  : xiangyuejia@qq.com
+
+from aitool.datasets.nlp.classification.classtest import ClassTest1
+data = ClassTest1('.', train=True, download=True)
+ccc = 1
