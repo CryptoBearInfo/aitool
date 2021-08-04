cc1c73bf990ccf211f6854dfbef319810260f6ea
Yuejia Xiang <xiangyuejia@qq.com>
2021-08-04 19:47:54 +0800

    feat: will upgrade after 2 seconds and clean tmp files

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
-rm dist/*
+rm -r dist/ build/ *.egg-info/
 python setup.py bdist_wheel
 python setup.py sdist
-python -m twine upload dist/*
+python3 -m twine upload dist/*
+sleep 2
+pip install aitool --upgrade
+rm -r dist/ build/ *.egg-info/
