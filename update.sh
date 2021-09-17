c4c8911ca45b66d086c72a4ba84601392e5115e9
Yuejia Xiang <xiangyuejia@qq.com>
2021-09-17 11:58:45 +0800

    fix: add requirements

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 rm -r dist/ build/ *.egg-info/
 python setup.py bdist_wheel
 python setup.py sdist
-python3 -m twine upload dist/*
+python3 -m twine upload dist/*  --username yuejiaxiang --password 1qazxsw2\'pypi
 pip install aitool --upgrade
 rm -r dist/ build/ *.egg-info/
