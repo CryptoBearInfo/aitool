18d6cda3acd9ab29571c4b1321418be3ada4f3c0
Yuejia Xiang <xiangyuejia@qq.com>
2022-01-10 23:23:15 +0800

    feat: add split_part, get_pair

--- aitool/__init__.py
+++ aitool/__init__.py
@@ -41,6 +41,8 @@ from aitool.basic_function.file import prepare_data as prepare_data
 from aitool.basic_function.format_data import flatten as flatten
 from aitool.basic_function.format_data import html2text as html2text
 from aitool.basic_function.format_data import content2text as content2text
+from aitool.basic_function.format_data import split_part as split_part
+from aitool.basic_function.format_data import get_pair as get_pair
 
 from aitool.basic_function.singleton import singleton as singleton
 from aitool.basic_function.exe_time import exe_time as exe_time
