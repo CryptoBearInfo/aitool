52e202afcbe7d01bceea1e239876ddc7da481228
xiangyuejia <xiangyuejia@qq.com>
2023-04-19 21:15:15 +0800

    fix: init file

--- aitool/__init__.py
+++ aitool/__init__.py
@@ -4,112 +4,54 @@ from aitool.datasets import PATH as DATAPATH
 from aitool.datasets import WEB as WEBPATH
 
 # BASIC FUNCTION
-from aitool.basic_function.basic import split_dict
-from aitool.basic_function.basic import replace_char
-from aitool.basic_function.basic import split_char
-from aitool.basic_function.basic import split_punctuation
-from aitool.basic_function.basic import is_appear
+from aitool.basic_function.display import print_color, print_red, print_green, print_yellow, print_blue
+from aitool.basic_function.singleton import singleton
+from aitool.basic_function.pip_tool import pip_install_by_os, pip_install_by_main, pip_install
 
-from aitool.basic_function.string import find_all_position
-from aitool.basic_function.string import get_ngram
-from aitool.basic_function.string import token_hit
-
+from aitool.basic_function.basic import split_dict, replace_char, split_char, split_punctuation, is_appear
+from aitool.basic_function.deduplication import Deduplication, deduplicate
+from aitool.basic_function.string_trans import find_all_position, get_ngram, get_ngrams, token_hit, filter_keyword
+from aitool.basic_function.distribution import normalize, cross_entropy, scale_array
 from aitool.basic_function.security import encrypt_md5
-
-from aitool.basic_function.deduplication import Deduplication
-from aitool.basic_function.deduplication import deduplicate
-
-from aitool.basic_function.path import get_user_root_path as get_user_root_path
-
-from aitool.basic_function.random import random_base64 as random_base64
-
-from aitool.basic_function.file import is_writable as is_writable
-from aitool.basic_function.file import file_exist as file_exist
-from aitool.basic_function.file import is_file_exist as is_file_exist
-from aitool.basic_function.file import is_file as is_file
-from aitool.basic_function.file import is_folder as is_folder
-from aitool.basic_function.file import make_dir as make_dir
-from aitool.basic_function.file import is_file_hidden as is_file_hidden
-from aitool.basic_function.file import get_file as get_file
-from aitool.basic_function.file import add_python_path as add_python_path
-
-from aitool.basic_function.file import dump_json as dump_json
-from aitool.basic_function.file import load_json as load_json
-from aitool.basic_function.file import dump_pickle as dump_pickle
-from aitool.basic_function.file import load_pickle as load_pickle
-from aitool.basic_function.file import dump_lines as dump_lines
-from aitool.basic_function.file import load_big_data as load_big_data
-from aitool.basic_function.file import load_line as load_line
-from aitool.basic_function.file import load_lines as load_lines
-from aitool.basic_function.file import dump_excel as dump_excel
-from aitool.basic_function.file import load_excel as load_excel
-from aitool.basic_function.file import dump_csv as dump_csv
-from aitool.basic_function.file import load_csv as load_csv
-
-from aitool.basic_function.file import zip as zip
-from aitool.basic_function.file import unzip as unzip
-from aitool.basic_function.file import split_path as split_path
-
-
-from aitool.basic_function.download.download import download_file as download_file
-from aitool.basic_function.download.download import get_download_dir as get_download_dir
-from aitool.basic_function.download.download import get_aitool_data_path as get_aitool_data_path
-from aitool.basic_function.download.download import prepare_data as prepare_data
-
-
-from aitool.basic_function.format_data import flatten as flatten
-from aitool.basic_function.format_data import html2text as html2text
-from aitool.basic_function.format_data import content2text as content2text
-from aitool.basic_function.format_data import split_part as split_part
-from aitool.basic_function.format_data import get_pair as get_pair
-from aitool.basic_function.format_data import np2list as np2list
-from aitool.basic_function.format_data import get_most_item as get_most_item
-
-from aitool.basic_function.singleton import singleton as singleton
-from aitool.basic_function.exe_time import exe_time as exe_time
+from aitool.basic_function.path import get_user_root_path
+from aitool.basic_function.random_str import random_base64
+from aitool.basic_function.file import is_writable, file_exist, is_file_exist, is_file, is_folder, make_dir, \
+    is_file_hidden, get_file, add_python_path
+from aitool.basic_function.file import dump_json, load_json, dump_pickle, load_pickle, dump_lines, load_big_data, \
+    load_byte, load_line, load_lines, dump_excel, load_excel, dump_csv, load_csv
+from aitool.basic_function.file import zip, unzip, split_path
+from aitool.basic_function.download.download import download_file, get_download_dir, get_aitool_data_path, prepare_data
+from aitool.basic_function.format_data import flatten, html2text, content2text, split_part, get_pair, np2list, \
+    get_most_item, dict2ranked_list
+from aitool.basic_function.word import is_contains_english, cut_until_char, delete_char, is_contains_figure, \
+    is_contains_chinese, is_all_chinese, is_nick_name
+from aitool.basic_function.exe_time import exe_time
 from aitool.basic_function.retry import retry
-from aitool.basic_function.time import timeout
-from aitool.basic_function.time import timestamp
+from aitool.basic_function.time import timeout, timestamp, get_lastday_timestamp
 
 # cache 管理工具
-from aitool.basic_function.cache import cache  # 装饰器
-from aitool.basic_function.cache import get_cache
-from aitool.basic_function.cache import Cache
+from aitool.basic_function.cache import cache, get_cache, Cache
 
 # 多进程
-from aitool.basic_function.multi import pool_map
-from aitool.basic_function.multi import pool_starmap
-from aitool.basic_function.multi import multi_map
-from aitool.basic_function.multi import get_functions
-from aitool.basic_function.multi import multi
+from aitool.basic_function.multi import pool_map, pool_starmap, multi_map, get_functions, multi
 
-from aitool.task_customized.ip_enhance.filter import has_family_name
-from aitool.task_customized.ip_enhance.filter import is_common_word
-from aitool.task_customized.ip_enhance.filter import is_stop_word
-from aitool.task_customized.ip_enhance.filter import is_relationship_title
-from aitool.task_customized.ip_enhance.filter import is_contains_english
-from aitool.task_customized.ip_enhance.filter import cut_until_char
-from aitool.task_customized.ip_enhance.filter import delete_char
-from aitool.task_customized.ip_enhance.filter import is_nick_name
-from aitool.task_customized.ip_enhance.filter import is_contains_figure
-from aitool.task_customized.ip_enhance.filter import delete_age_describe
-from aitool.task_customized.ip_enhance.filter import is_contains_chinese
-from aitool.task_customized.ip_enhance.filter import is_all_chinese
-from aitool.task_customized.ip_enhance.filter import is_black_name
-from aitool.task_customized.ip_enhance.filter import clean_role
-from aitool.task_customized.ip_enhance.filter import clean_alias
-from aitool.task_customized.ip_enhance.filter import delete_nested_text
-from aitool.task_customized.ip_enhance.filter import select_nested_text
-from aitool.task_customized.ip_enhance.filter import is_sub_ip
-from aitool.task_customized.ip_enhance.filter import get_core_ip
-from aitool.task_customized.keyword_mining.keyword import get_keyword_graph
-from aitool.task_customized.keyword_mining.keyword import get_keyword_graph4panda
-from aitool.task_customized.keyword_mining.keyword import get_keyword
-from aitool.task_customized.keyword_mining.keyword import SentenceKeyword
-
+# ARITHMETIC FUNCTION
+from aitool.data_structure.arithmetic.dfs_search import Node, dfs, ranked_permutation
 
 # NLP FUNCTION
 from aitool.nlp.basic.split_sentence import split_sentence
-from aitool.nlp.sentiment_analysis.dict_match import Sentiment
-
 from aitool.nlp.basic.conditional_probability import conditional_probability
+from aitool.nlp.basic.phoneticize import get_pinyin
+from aitool.nlp.basic.ngram_tf_idf import get_ngram_tf, get_ngram_idf
+from aitool.nlp.sentiment_analysis.dict_match import Sentiment
+from aitool.nlp.sentiment_analysis.text_similar import load_word2vec, cos_sim, VectorSim, vector_sim, char_sim, de_sim, \
+    generate_offline_sim
+from aitool.nlp.chatgpt.chatgpt import chatgpt
+
+# knowledge_graph
+from aitool.knowledge_graph.paris.examples.core_example import alignment
+
+# task_customized
+from aitool.task_customized.ip_enhance.filter import has_family_name, is_common_word, is_stop_word, \
+    is_relationship_title, delete_age_describe, is_black_name, clean_role, clean_alias, delete_nested_text, \
+    select_nested_text, is_sub_ip, get_core_ip
